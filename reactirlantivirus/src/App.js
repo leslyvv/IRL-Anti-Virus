291c2b20afe46bb7d963643e89630a88d1807df8
dwijvas <dwij.vasisth@gmail.com>
2024-03-17 10:42:22 -0400

    signup frontend initial

--- reactirlantivirus/src/App.js
+++ reactirlantivirus/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from "./components/Login"; 
+import Signup from "./components/Signup"; 
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} /> {/* Changed 'login' to 'Login' */}
+        <Route path="/signup" element={<Signup />} /> {/* Changed 'login' to 'Login' */}
+
       </Routes>
     </div>
   );
