79dadb84380287257adc0ef4cf279dbcb8ed2340
Lesly Vargas <117410261+leslyvv@users.noreply.github.com>
2024-03-27 02:07:00 -0400

    Forgot password pages

---
+++ reactirlantivirus/src/components/Forgot_Password.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import './Forgot_password.css'; 
+const Forgot_Password = () => {
+
+  const[email, setEmail] = useState('');
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email);
+    navigate('/forgot-password-success');
+  };
+  return(
+    <form onSubmit={handleSubmit}>
+      <div className="forgot-password-header">Forgot your password?
+        <div className= "email-block"><span className= "email-text">Email</span>
+        <input className="email-box" type="text" name="email" value={email} onChange={handleChange} pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" placeholder="example@.com" required />
+        </div>
+        <button className="reset-button">Reset Password</button>
+        <p className="cancel-link">
+            <a href="#">Cancel</a>
+        </p>
+      </div>
+    </form>
+  );
+  };
+
+export default Forgot_Password;
---
+++ reactirlantivirus/src/components/Forgot_Password_Success.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './Forgot_password.css'; 
+const Forgot_Password_Success = () => {
+  
+  return(
+    <div className= "pass-reset-container">
+      <span className ="pass-header">Password Reset</span>
+      <p className = "pass-reset-info">
+      We have sent you an e-mail. Please contact us if you do not receive it in a few minutes.
+      </p>
+    </div>
+  )
+}
+export default Forgot_Password_Success;
