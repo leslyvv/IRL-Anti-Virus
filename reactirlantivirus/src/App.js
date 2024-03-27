bd0c602c6c057a8571e25f5a369eb3b423f00b6f
Lesly Vargas <117410261+leslyvv@users.noreply.github.com>
2024-03-27 02:19:39 -0400

    Update App.js

--- reactirlantivirus/src/App.js
+++ reactirlantivirus/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from "./components/Login"; 
 import Signup from "./components/Signup"; 
+import Forgot_Password from "./components/Forgot_Password"
+import Forgot_Password_Success from "./components/Forgot_Password_Success"
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} /> {/* Changed 'login' to 'Login' */}
         <Route path="/signup" element={<Signup />} /> {/* Changed 'login' to 'Login' */}
+        <Route path="/forgot-password" element={<Forgot_Password/> }/>
+        <Route path="/forgot-password-success" element={<Forgot_Password_Success/>}/>
 
       </Routes>
     </div>
