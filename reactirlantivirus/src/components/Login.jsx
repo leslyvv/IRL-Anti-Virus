dbbc2db886fe8887025b2ab98b97ef5db5067b0a
Lesly Vargas <117410261+leslyvv@users.noreply.github.com>
2024-03-28 02:26:54 -0400

    Update Login.jsx

--- reactirlantivirus/src/components/Login.jsx
+++ reactirlantivirus/src/components/Login.jsx
@@ -1,35 +1,45 @@
-import React from 'react';
-import {Link} from "react-router-dom"
+import React, {useState} from 'react';
+import {Link, useNavigate} from "react-router-dom"
 import userIcon from './user_icon.png'; // Import the user icon image
 import keyIcon from './key_pwd.png';
 import './Login.css'; // Import your CSS file
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState(''); 
+  const navigate = useNavigate(); 
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log("Username: ", username," Password: ", password);
+  }
   return (
     <div className="account-container">
-      <form>
+      <form onSubmit={handleLogin}>
           <div className="login-header">
             Login
           </div>
           <div className="form-container">
             <p className="username-section">
-              <input className="user-box" type="text" placeholder="Enter Username" pattern="^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$" required />
+              <input className="user-box" type="text" placeholder="Enter Username" pattern="^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$" 
+                value = {username} required onChange= {(e) => setUsername(e.target.value)}/>
               <img className="user-pic" src={userIcon} alt="User Icon" />
             </p>
             <p className="password-section">
-              <input className="password-box" type="password" placeholder="Enter Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}" required />
+              <input className="password-box" type="password" placeholder="Enter Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}" 
+              value = {password} required onChange= {(e) => setPassword(e.target.value)} />
               <img className="key-pic" src={keyIcon} alt="Key Icon" />
             </p>
           </div>
           <div className="form-subcontainer">
             <p className="forgot-password">
-              <Link to ="/forgot-password">Forgot password?</Link>
+              <Link className= "shadowing" to ="/forgot-password">Forgot password?</Link>
             </p>
           </div>
           <div className="submission">
             <button className="login-button">Login</button>
             <p className="register">
-              Not a registered user? <a href="signup">Register here!</a>
+              Not a registered user? <Link className= "shadowing" to ="/signup">Register here!</Link>
             </p>
           </div>        
       </form>
