c95c4082f2ee2398af49133964d2e2759b80c1fc
Zion2-6 <113418641+Zion2-6@users.noreply.github.com>
2024-04-01 16:05:55 -0400

    Model - Class Prescription

--- IRLVirus/djangox/pages/models.py
+++ IRLVirus/djangox/pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Prescription(models.Model):
+    prescription_id = models.IntegerField
+    doctor_id = models.IntegerField
+    patient_id = models.IntegerField
+    medicine_name = models.CharField(max_length=30)
+    dosage_desc = models.CharField(max_length=30)
+    prescription_fee = models.FloatField
+    additional_notes = models.CharField(max_length=255)
+    
